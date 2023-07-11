71ff4e1c7371b467a9cbc170c2bb37308952be40
ArmandoLluen <92061855+ArmandoLluen@users.noreply.github.com>
2023-07-10 19:59:20 -0500

    subir examen

--- Pregunta3/Pregunt3.py
+++ Pregunta3/Pregunt3.py
@@ -19,39 +19,41 @@ with mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence
         # Realizar la detección de la malla facial con MediaPipe
         results = face_mesh.process(image_rgb)
 
-        # Dibujar los puntos clave y las conexiones en la imagen
         if results.multi_face_landmarks:
             for face_landmarks in results.multi_face_landmarks:
-                # Obtener las coordenadas de los labios
-                lip_landmarks = face_landmarks.landmark[mp_face_mesh.FaceLandmark.UPPER_LIP_TOP:mp_face_mesh.FaceLandmark.LOWER_LIP_BOTTOM + 1]
+                # Obtener los índices de los puntos clave de los labios
+                upper_lip_indices = [61, 62, 63, 64, 65, 185, 92, 186]
+                lower_lip_indices = [39, 40, 41, 42, 43, 44, 78, 95]
 
                 # Dibujar los puntos clave de los labios
-                for landmark in lip_landmarks:
+                for index in upper_lip_indices + lower_lip_indices:
+                    landmark = face_landmarks.landmark[index]
                     x = int(landmark.x * frame.shape[1])
                     y = int(landmark.y * frame.shape[0])
                     cv2.circle(frame, (x, y), 2, (0, 255, 0), -1)
 
                 # Dibujar las líneas que conectan los puntos clave de los labios
-                for i in range(len(lip_landmarks) - 1):
-                    x1 = int(lip_landmarks[i].x * frame.shape[1])
-                    y1 = int(lip_landmarks[i].y * frame.shape[0])
-                    x2 = int(lip_landmarks[i + 1].x * frame.shape[1])
-                    y2 = int(lip_landmarks[i + 1].y * frame.shape[0])
+                for i in range(len(upper_lip_indices) - 1):
+                    x1 = int(face_landmarks.landmark[upper_lip_indices[i]].x * frame.shape[1])
+                    y1 = int(face_landmarks.landmark[upper_lip_indices[i]].y * frame.shape[0])
+                    x2 = int(face_landmarks.landmark[upper_lip_indices[i + 1]].x * frame.shape[1])
+                    y2 = int(face_landmarks.landmark[upper_lip_indices[i + 1]].y * frame.shape[0])
                     cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-                # Reconocer las vocales (a, e, i, o, u) basadas en la posición de los labios
-                lip_distance = lip_landmarks[6].y - lip_landmarks[0].y
+                for i in range(len(lower_lip_indices) - 1):
+                    x1 = int(face_landmarks.landmark[lower_lip_indices[i]].x * frame.shape[1])
+                    y1 = int(face_landmarks.landmark[lower_lip_indices[i]].y * frame.shape[0])
+                    x2 = int(face_landmarks.landmark[lower_lip_indices[i + 1]].x * frame.shape[1])
+                    y2 = int(face_landmarks.landmark[lower_lip_indices[i + 1]].y * frame.shape[0])
+                    cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 1)
 
-                if lip_distance > 0.03:
-                    cv2.putText(frame, 'Vocal: A', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif lip_distance > -0.01:
-                    cv2.putText(frame, 'Vocal: E', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif lip_distance > -0.06:
-                    cv2.putText(frame, 'Vocal: I', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                elif lip_distance > -0.1:
-                    cv2.putText(frame, 'Vocal: O', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                upper_lip_distance = face_landmarks.landmark[61].y - face_landmarks.landmark[185].y
+                lower_lip_distance = face_landmarks.landmark[39].y - face_landmarks.landmark[44].y
+
+                if upper_lip_distance > 0.002 and lower_lip_distance > 0.002:
+                    cv2.putText(frame, 'vocal abierta', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
                 else:
-                    cv2.putText(frame, 'Vocal: U', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    cv2.putText(frame, 'Vocal cerrada', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
 
         # Mostrar la imagen en una ventana
